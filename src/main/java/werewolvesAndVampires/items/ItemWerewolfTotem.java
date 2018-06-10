7175561c5b9958395e21e0958e83c3632d877dfb
Daniel Hajjar <daniel.hajjar@hotmail.se>
2018-06-10 21:14:31 +0200

    Added Werewolf totem

---
+++ src/main/java/werewolvesAndVampires/items/ItemWerewolfTotem.java
@@ -0,0 +1,12 @@
+package werewolvesAndVampires.items;
+
+import net.minecraft.creativetab.CreativeTabs;
+
+public class ItemWerewolfTotem extends BaseItem {
+	
+	public ItemWerewolfTotem() {
+		super("werewolf_totem");
+		this.setCreativeTab(CreativeTabs.MISC);
+	}
+	
+}
